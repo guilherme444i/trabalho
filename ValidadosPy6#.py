06360c827511a364ce082c80cc3a6af328b02088
guilherme444i <166552605+guilherme444i@users.noreply.github.com>
2024-05-03 15:12:04 -0300

    Add files via upload

---
+++ ValidadosPy6#.py
@@ -0,0 +1,5 @@
+dicionario = { 'time':'tempo','people':'pessoas','way':'modo','water':'agua','words':'palavras','man':'homem','part':'parte','place':'lugar', 'thing':'coisas', 'years':'anos','number':'numero','name':'nome','mister':'senhor','air':'ar','line':'linha','end':'final','sound':'som','world':'mundo','school':'escola','food':'comida'}
+
+for English, Portugues in dicionario.items():
+    print(f'Ingles: {English} Portugues: {Portugues}')
+
